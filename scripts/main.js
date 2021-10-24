36431d1bd23693b776b372a598e67776d9db5fbd
Venn Wylde <venn@workplaydance.com>
2021-10-24 17:31:56 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -33,9 +33,10 @@ $(function() {
 
     // Populate using a JSON file
     // WTF.init( 'sample.json' );
-
+    WTF.init( 'fourclassparty.json' );
+    
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '1j8HB7nNifoL5j0eWmzbNCpCJrdR40Oahfylg9Iqxs8Y' );
+    // WTF.init( '1j8HB7nNifoL5j0eWmzbNCpCJrdR40Oahfylg9Iqxs8Y' );
 
 });
